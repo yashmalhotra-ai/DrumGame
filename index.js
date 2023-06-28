0966d95e1ec1a652090be1c4e07921455bb0760e
yashmalhotra-ai <captainyashiphone@gmail.com>
2023-06-29 03:12:45 +0530

    Adding existing project to Github

---
+++ index.js
@@ -0,0 +1,53 @@
+let len = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < len; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let btninnerHTML = this.innerHTML;
+        Sounds(btninnerHTML);
+    })
+}
+
+document.addEventListener("keypress",function(event){
+    Sounds(event.key);
+})
+
+function Sounds(key){
+    
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3")
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3")
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3")
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3")
+            audio.play();
+            break;
+        default:
+            console.log(btninnerHTML);
+            break;
+    }
+}
